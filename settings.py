253b51ce423f32d4214f278f7d6d84af3492b84b
megumi tree <torii.megumi@gmail.com>
2023-06-30 14:48:39 +0900

    Update settings.py

--- settings.py
+++ settings.py
@@ -5,13 +5,13 @@
 
 import os
 from os.path import join, dirname
-from dotenv import load_dotenv
-load_dotenv(verbose=True)
+# from dotenv import load_dotenv
+# load_dotenv(verbose=True)
 
 # settings.pyの相対パスを取得し、.envファイルを指定
-dotenv_path = join(dirname(__file__), '.env')
+# dotenv_path = join(dirname(__file__), '.env')
 # ファイルの中身を読み取る
-load_dotenv(dotenv_path)
+# load_dotenv(dotenv_path)
 
 # 環境変数を参照
 TOKEN = os.environ.get("TOKEN")
